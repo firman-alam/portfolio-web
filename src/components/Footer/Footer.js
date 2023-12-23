222eb122b6cb462f8136dbd243b286c73845ef7c
lintek.dev02 <lintek.dev02@lintekindo.com>
2023-12-23 11:07:17 +0700

    update link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-import { FaSpotify, FaGoodreads, FaTwitter } from 'react-icons/fa';
+import { FaSpotify, FaGoodreads, FaTwitter, FaMedium } from "react-icons/fa"
 import { SocialIcons } from '../Header/HeaderStyles';
 import {
   CompanyContainer,
@@ -20,7 +20,7 @@ const Footer = () => {
       <LinkList>
         <LinkColumn>
           <LinkTitle>Email</LinkTitle>
-          <LinkItem href='mailto:aladiat046@gmail.com'>
+          <LinkItem href="mailto:aladiat046@gmail.com">
             aladiat046@gmail.com
           </LinkItem>
         </LinkColumn>
@@ -30,28 +30,31 @@ const Footer = () => {
           <Slogan>Learn. Unlearn. Relearn.</Slogan>
         </CompanyContainer>
         <SocialContainer>
-          <SocialIcons href='https://github.com/firman-alam'>
-            <AiFillGithub size='3rem' />
+          <SocialIcons href="https://github.com/firman-alam">
+            <AiFillGithub size="3rem" />
           </SocialIcons>
-          <SocialIcons href='https://www.linkedin.com/in/al-adiat-firman-alamsyah-279222217/'>
-            <AiFillLinkedin size='3rem' />
+          <SocialIcons href="https://www.linkedin.com/in/al-adiat-firman-alamsyah-279222217/">
+            <AiFillLinkedin size="3rem" />
           </SocialIcons>
-          <SocialIcons href='https://www.instagram.com/firmann.alam/'>
-            <AiFillInstagram size='3rem' />
+          <SocialIcons href="https://www.instagram.com/firmann.alam/">
+            <AiFillInstagram size="3rem" />
           </SocialIcons>
-          <SocialIcons href='https://open.spotify.com/user/21ukddgo4xikipsvkoxkvktja?si=bfa5X1_xQaiMLE6yg67IRQ&utm_source=whatsapp'>
-            <FaSpotify size='2.6rem' />
+          <SocialIcons href="https://open.spotify.com/user/21ukddgo4xikipsvkoxkvktja?si=bfa5X1_xQaiMLE6yg67IRQ&utm_source=whatsapp">
+            <FaSpotify size="2.6rem" />
           </SocialIcons>
-          <SocialIcons href='https://www.goodreads.com/firmann_alam'>
-            <FaGoodreads size='3rem' />
+          <SocialIcons href="https://www.goodreads.com/firmann_alam">
+            <FaGoodreads size="3rem" />
           </SocialIcons>
-          <SocialIcons href='https://twitter.com/firmann_alam'>
-            <FaTwitter size='3rem' />
+          <SocialIcons href="https://twitter.com/firmann_alam">
+            <FaTwitter size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://medium.com/@aladiat046">
+            <FaMedium size="3rem" />
           </SocialIcons>
         </SocialContainer>
       </SocialIconsContainer>
     </FooterWrapper>
-  );
+  )
 };
 
 export default Footer;
